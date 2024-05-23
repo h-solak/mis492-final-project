2e96e54989b073ebd7cd1f281ba0dc99bb65f99c
Hasan Solak <hasan.solak@kobil.com>
2024-05-23 11:43:58 +0300

    api change fe

--- src/api/config.js
+++ src/api/config.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 //Our API config
 const misBaseAxios = axios.create({
-  baseURL: "http://localhost:5900/api",
-  // baseURL: "https://mis492-final-project.onrender.com/api",
+  // baseURL: "http://localhost:5900/api",
+  baseURL: "https://mis492-final-project.onrender.com/api",
+  //
   headers: {
     "Content-Type": "application/json",
   },
